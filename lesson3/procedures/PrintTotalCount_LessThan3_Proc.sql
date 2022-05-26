701533df374fdd828e62dcb068c13a93b624b400
Ivan Korchmit <criperadmiral92@gmail.com>
2022-05-26 15:10:46 +0300

    New IF ELSE Proc

---
+++ lesson3/procedures/PrintTotalCount_LessThan3_Proc.sql
@@ -0,0 +1,16 @@
+create proc spGetEmployeeCountByGender
+@Gender nvarchar(20),
+@EmployeeCount int output
+as begin
+	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
+end
+
+
+-- annab teada, palju on meessoost isikuid ning kuvab vastava stringi
+declare @TotalCount int
+exec spGetEmployeeCountByGender 'Female', @TotalCount out
+if(@TotalCount = 0)
+	print '@TotalCount is null'
+else
+	print '@TotalCount is not null'
+print @TotalCount
