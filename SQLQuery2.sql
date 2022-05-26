a358c39d266ed3b65f53ccd35a44ac48f80f4dd2
alexkaasik <aleks.kaasik@outlook.com>
2022-05-26 15:01:40 +0300

    print first name in the alphabet names of the worker

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,111 +1,84 @@
+--- loome stored procedure, mis kuvab vaate
+create procedure spGetEmployees
+as begin
+	select FirstName, Gender from Employees
+end
 
---- loome uued tabelid
-create table Department
-(
-Id int primary key,
-DepartmentName nvarchar(50),
-Location nvarchar(50),
-DepartmentHead nvarchar(50)
-)
+spGetEmployees
+exec spGetEmployees
+execute spGetEmployees
 
-create table Employees
-(
-Id int primary key,
-Name nvarchar(50),
-Gender nvarchar(10),
-Salary nvarchar(50),
-DepartmentId int
-)
+--- 
+create proc spGetEmployeesByGenderAndDepartment
+@Gender nvarchar(20),
+@DepartmentId int
+as begin
+	select FirstName, Gender, DepartmentId from Employees where Gender = @Gender
+	and DepartmentId = @DepartmentId
+end
 
----sisestame andmed
-insert into Department (Id, DepartmentName, Location, DepartmentHead)
-values (1, 'IT', 'London', 'Rick')
-insert into Department (Id, DepartmentName, Location, DepartmentHead)
-values (2, 'Payroll', 'Delhi', 'Ron')
-insert into Department (Id, DepartmentName, Location, DepartmentHead)
-values (3, 'HR', 'New York', 'Christie')
-insert into Department (Id, DepartmentName, Location, DepartmentHead)
-values (4, 'Other Deparment', 'Sydney', 'Cindrella')
+--- kõik esimeses osakonnas meessoost töötavad isikud
+spGetEmployeesByGenderAndDepartment 'Male', 1
 
-select * from Department
-
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (1, 'Tom', 'Male', 4000, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (2, 'Pam', 'Female', 3000, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (3, 'John', 'Male', 3500, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (4, 'Sam', 'Male', 4500, 2)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (5, 'Todd', 'Male', 2800, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (6, 'Ben', 'Male', 7000, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (7, 'Sara', 'Female', 4800, 3)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (8, 'Valarie', 'Female', 5500, 1)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (9, 'James', 'Male', 6500, NULL)
-insert into Employees (Id, Name, Gender, Salary, DepartmentId)
-values (10, 'Russell', 'Male', 8800, NULL)
-
-select * from Employees
-
---- saan ainult selles tabelis olevad veerud
-select distinct Name, DepartmentId from Employees
-
---- arvutame k]ikide palgad kokku
-select sum(cast(Salary as int)) from Employees
---- k]ige v'iksema palga saaja
-select min(cast(Salary as int)) from Employees
-
-
-alter table Employees add City varchar(25)
-
-
-alter table Employees
-add DepartmentId
-int null
-
-
---- lisame tabelid
-alter table Employees
-add FirstName nvarchar(30)
-
-alter table Employees
-add MiddleName nvarchar(30)
-
-alter table Employees
-add LastName nvarchar(30)
+spGetEmployeesByGenderAndDepartment @DepartmentId =  1, @Gender = 'Male'
 
 
 
-update Employees set FirstName = 'Tom', MiddleName = 'Nick', LastName = 'Jones'
-where Id = 1
-update Employees set FirstName = 'Pam', MiddleName = NULL, LastName = 'Anderson'
-where Id = 2
-update Employees set FirstName = 'John', MiddleName = NULL, LastName = NULL
-where Id = 3
-update Employees set FirstName = 'Sam', MiddleName = NULL, LastName = 'Smith'
-where Id = 4
-update Employees set FirstName = NULL, MiddleName = 'Todd', LastName = 'Someone'
-where Id = 5
-update Employees set FirstName = 'Ben', MiddleName = 'Ten', LastName = 'Sven'
-where Id = 6
-update Employees set FirstName = 'Sara', MiddleName = NULL, LastName = 'Connor'
-where Id = 7
-update Employees set FirstName = 'Valarie', MiddleName = 'Balerine', LastName = NULL
-where Id = 8
-update Employees set FirstName = 'James', MiddleName = '007', LastName = 'Bond'
-where Id = 9
-update Employees set FirstName = NULL, MiddleName = NULL, LastName = 'Crowe'
-where Id = 10
+
+create proc spGetEmployeeCountByGender
+@Gender nvarchar(20),
+@EmployeeCount int output
+as begin
+	select @EmployeeCount = count(Id) from Employees where Gender = @Gender
+end
+
+-- annab teada, palju on meessoost isikuid ning kuvab vastava stringi
+declare @TotalCount int
+exec spGetEmployeeCountByGender 'Female', @TotalCount out
+if(@TotalCount = 0)
+	print '@TotalCount is null'
+else
+	print '@TotalCount is not null'
+print @TotalCount
+
+-- annab teada, palju on meessoost isikuid
+declare @TotalCount int
+exec spGetEmployeeCountByGender @EmployeeCount = @TotalCount out, @Gender = 'Male'
+print @TotalCount
+
+---- annab kogu tabeli ridade arvu
+create proc spTotalCount2
+@TotalCount int output
+as begin
+	select @TotalCount = count(Id) from Employees
+end
+--- käivitame sp
+declare @TotalEmployees int
+execute spTotalCount2 @TotalEmployees output
+select @TotalEmployees
+
+--- mis id all on keegi nime järgi
+create proc spGetNameById1
+@Id int,
+@FirstName nvarchar(50) output
+as begin
+	select @FirstName = FirstName from employees where Id = @Id
+end
+
+--- käivitame sp
+declare @FirstName nvarchar(50)
+execute spGetNameById1 6, @FirstName output
+print 'Name of the employee = ' + @FirstName
 
 
---- igast reast võtab esimeses veerus täidetud lahtri ja kuvab ainult seda
-select Id, coalesce(FirstName, MiddleName, LastName) as Name
-from Employees
+create proc spGetNameById2
+@Id int
+as begin
+	return (select FirstName from Employees where Id = @Id)
+end
+-- tuleb veatead kuna kutsusime välja int-i, aga Tom on string
+declare @EmployeeName nvarchar(50)
+exec @EmployeeName = spGetNameById2 1
+print 'Name of the employee = ' + @EmployeeName
 
 select * from Employees
-select * from Department
