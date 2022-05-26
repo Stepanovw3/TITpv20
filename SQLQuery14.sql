97065318043c860f27c61f7b2bfa9abb17f77fcf
georg <daniil.kuzjomin@gmail.com>
2022-05-26 15:30:45 +0300

    все сотрудники мужского пола в первом отделе

---
+++ SQLQuery14.sql
@@ -0,0 +1,7 @@
+create proc spGetEmployeesByGenderAndDepartment
+@Gender nvarchar(20),
+@DepartmentId int
+as begin
+	select FirstName, Gender, DepartmentId from Employees where Gender = @Gender
+	and DepartmentId = @DepartmentId
+end
